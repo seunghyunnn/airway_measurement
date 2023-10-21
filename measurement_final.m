ce6b75d2c35d8dd5ade77d16b1776aaf8c208371
seunghyunnn <rofam2@naver.com>
2023-10-21 13:32:19 +1100

    MATLAB code for generating the measurement and saving it to a CSV file

---
+++ measurement_final.m
@@ -0,0 +1,22 @@
+%PATH setups 
+path = "/Users/seunghyun/comp3888/AirQuant/";
+addpath(path + "/library/util");
+addpath(path);
+AirQuantDir = AirQuantAddPath();
+
+%Load DATA
+CT_name = path + "ct100.nii";
+seg_name = path + "gt100.nii";
+skel_name = path + "skel100at600.nii";
+meta = niftiinfo(CT_name);
+source = double(niftiread(meta));
+seg = logical(niftiread(seg_name));
+skel = logical(niftiread(skel_name));
+
+%Create AQnet and retrieve measurement 
+num_rays = 799; %60; 
+ray_interval = 0.5;%0.2;
+AQnet = ClinicalAirways(skel, source=source, header=meta, seg=seg, fillholes=1, largestCC=1, plane_sample_sz=0.5, spline_sample_sz=0.5);
+AQnet.MakeTubePatches(method='linear', gpu=0);
+AQnet.Measure('AirwayFWHMesl', num_rays, ray_interval);
+AQnet.ExportCSV('example600.csv');
